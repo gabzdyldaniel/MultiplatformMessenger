9a7cf2eef8fef2a172440fdb979b63a9fa3f871b
Daniel Gabzdyl <daniel.gabzdyl@b2a.cz>
2022-05-13 08:17:00 +0200

    add custom AppDelegate

---
+++ Shared/AppMain.swift
@@ -0,0 +1,29 @@
+//
+//  AppMain.swift
+//  MultiplatformMessenger
+//
+//  Created by Daniel Gabzdyl on 13.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppMain: App {
+
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+
+        UserService.shared.initUserFromDefaults()
+
+        return true
+    }
+}
